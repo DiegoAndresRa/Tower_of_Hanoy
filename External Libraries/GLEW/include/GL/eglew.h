3992e9e61df1b9f8c9df92424fccf9e68902ca4a
DiegoAndresRa <diego.unam.15@gmail.com>
2023-12-06 18:33:55 -0600

    add: libraries

---
+++ External Libraries/GLEW/include/GL/eglew.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a083a4e57554aa33907722e00ba129895554bfc9f7c73fc3648c68577046c5f4
+size 103594
---
+++ External Libraries/assimp/include/assimp/config.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:805dade91190c846b489375b07e1d1b6cef0835b346346f7b403bc7ac50b022b
+size 40119
